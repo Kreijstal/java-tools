9db7ae86e98ca37436127bca85220c0aee257b24
Kreijstal <rainb@tfwno.gf>
2024-10-05 00:15:46 +0200

    feat: add classPath parameter to loadClass function for enhanced class loading functionality

--- src/classLoader.js
+++ src/classLoader.js
@@ -1,5 +1,12 @@
-function loadClass(className) {
+function loadClass(className,classPath) {
+
   console.log(`Attempt to load class ${className}`);
+//if the class name path starts with java we ignore it for now
+  //we look into our classpath and attempt to find the file
+  const classFileContent = fs.readFileSync(classFilePath);
+  const ast = getAST(new Uint8Array(classFileContent));
+  const convertedAst = convertJson(ast.ast, ast.constantPool);
+  return convertedAst;
 }
 
 module.exports = { loadClass };
