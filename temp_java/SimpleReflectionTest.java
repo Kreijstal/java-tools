3267a905ef874b934aee4c41753acc99ab25aae9
Kreijstal <rainb@tfwno.gf>
2025-08-15 17:00:37 +0000

    looks different

--- temp_java/SimpleReflectionTest.java
+++
@@ -1,8 +0,0 @@
-public class SimpleReflectionTest {
-    public static void main(String[] args) throws Exception {
-        Class<?> targetClass = SimpleTarget.class;
-        java.lang.reflect.Method method = targetClass.getMethod("simpleStaticMethod");
-        Object result = method.invoke(null);
-        System.out.println(result);
-    }
-}
