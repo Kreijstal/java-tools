fb9de50a0bc559c666b2d7c9f2ea39db77e74f61
Kreijstal <rainb@tfwno.gf>
2026-08-25 06:40:17 +0200

    Revert "Expose structured Wasm to browser launchers"
    This reverts commit 3d30b6fc1f65619c45c432ebca60bdaa59ded0ee.

--- src/jit/JitCompiler.js
+++ src/jit/JitCompiler.js
@@ -470,7 +470,7 @@ class JitCompiler {
     // so latch the instrumentation flags once.
     this._envInstrumented = Boolean(typeof process !== "undefined" && process.env &&
       (process.env.JVM_TRACE || process.env.JVM_PROFILE_HOT_METHODS === "1"));
-    this.wasmJit = new WasmJit(jvm, this, options);
+    this.wasmJit = new WasmJit(jvm, this);
     const regionOptions = this.rendererPipelineEnabled
       ? { ...options, structuredSsa: true }
       : options;
